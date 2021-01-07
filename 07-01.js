3421a85753d7c2512ab71764edf76fe8c45d31ed
Roman Lebedenko <ceobit90@gmail.com>
2021-01-07 23:01:42 +0100

    assignment 07-01 v.01

--- 07-01.js
+++ 07-01.js
@@ -1,3 +1,16 @@
-// Create a program that prints a random integer from 1 - 6.
+// A parity bit, or check bit, is a bit added to a string of bits to ensure that the total number of 1-bits in the string is even or odd. Parity bits are used as the simplest form of error detecting code.
+//
+//   You have two parameters, one being the wanted parity (always 'even' or 'odd'), and the other being the binary representation of the number you want to check.
+//
+//   Your task is to return an integer (0 or 1), whose the parity bit you need to add to the binary representation so that the parity of the resulting string is as expected.
 
-console.log(Math.floor(Math.random() * 6)+1);
+function checkParity(parity, bin) {
+  const result = bin.split("1").length - 1 % 2 === 0 ? parity === 'even' ? 0 : 1 : parity === 'even' ? 1 : 0;
+  console.log(result);
+}
+
+checkParity('even','101010'); //1;
+checkParity('odd','101010'); // 0;
+checkParity('even','101011'); //0;
+checkParity('odd','101011'); // 1
+
