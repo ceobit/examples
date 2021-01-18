596660a01c85b3ff527e2834b09b28288142a231
Roman Lebedenko <ceobit90@gmail.com>
2021-01-18 14:49:55 +0100

    assignment 18-01

--- 18-01.js
+++ 18-01.js
@@ -47,3 +47,57 @@ const checkAge = (firstName, age) => {
 const firstName = 'Roman';
 const age = 30;
 checkAge(firstName, age);
+
+// Capitalize.
+//   Create a program that capitalizes the first letter of each element in an array of names. Examples:
+// [“matt”, “sara”, “lara”] ➞ [“Matt”, “Sara”, “Lara”]
+// [“samuel”, “MARIA”, “luke”, “mary”] ➞ [“Samuel”, “Maria”, “Luke”, “Mary”]
+// [“Cynthia”, “Karen”, “Jane”, “Carrie”] ➞ [“Cynthia”, “Karen”, “Jane”, “Carrie”]
+
+const capitalize = arr => {
+  return arr.map(el => {
+    return el[0].toUpperCase() + el.slice(1);
+  });
+}
+
+console.log(capitalize(['matt', 'sara', 'lara']));
+
+// City Names.
+//   Create an array of city names. Loop through the array and add the city names to a string. Examples:
+// [Berlin, Paris, Prague, Rome] ➞ expected output: “Berlin, Paris, Prague, Rome”.
+
+const fromArrToStr = str => {
+  return str.join(', ');
+}
+console.log(fromArrToStr(['Berlin', 'Paris', 'Prague', 'Rome']));
+
+// Hello
+// Create an array filled with your friends’ or family’s names. Loop over the array and greet each friend. Bonus: Print the indexes of each item in the array. Examples:
+// [Maria, Mike, Paul, Doven] ➞ expected output: “Hello Maria! Hello Mike! Hello Paul! Hello Doven!”
+// Bonus [Susan, Rezvane, Mark] ➞ expected bonus output: “Susan is at index 0 of my friends and family array, Rezvane is at index 1 of my friends and family array, Mark is at index 2 of my friends and family array”.
+const greeting = arr => {
+  let str = ''
+  let str2 = '';
+  arr.forEach((el, i) => {
+    str +=`Hello ${el}! `;
+    str2 +=`${el} is at index ${i} of my friends and family array, `;
+  });
+  console.log(str);
+  console.log(str2);
+}
+
+greeting(['Maria', 'Mike', 'Paul', 'Doven']);
+
+// Odds and Evens.
+//   Create a program that changes a given array by adding 1 to each odd integer and subtracting 1 from each even integer. Examples:
+// [3, 5, 2, 4] ➞ expected output: [4, 6, 1, 3]
+//   [6, 9, 10, 20] ➞ expected output: [5, 10, 9, 19]
+
+const plusOrMinus = arr => {
+  return arr.map(el => {
+    return el % 2 === 0 ? el - 1 : el + 1;
+  })
+}
+
+console.log(plusOrMinus([3, 5, 2, 4]));
+console.log(plusOrMinus([6, 9, 10, 20]));
