fb7f783816a177e016d393f0b2770e4431769527
Roman Lebedenko <ceobit90@gmail.com>
2021-01-19 21:48:18 +0100

    assignent 19-01

--- assigment 19-01.js
+++ assigment 19-01.js
@@ -3,58 +3,24 @@
 //   of vowels (i.e. in English, “a, e, i, o, u”) in the string.
 //   i.e. findVowels(“this is a string”)
 
+// first way
 const findVowels = str => {
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
-  const arr = str.split('');
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < vowels.length; j++) {
-      if (arr[i] === vowels[j]) {
-        count++;
-      }
-    }
-  }
-return count;
+  const vowels = /[aeiou]/; //regular expression
+  return str.split(vowels).length - 1;
 }
 
-console.log(findVowels('this is a striing'));
+console.log(findVowels('this is a string'));
 
-const findVowels2 = str => {
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
-  const arr = str.split('');
+// second way
+const findVowels1 = str => {
+  const vowels = ['a','e','i','o','u'];
+  const arrayFromStr = str.split('');
   let count = 0;
-  for (let i = 0; i < arr.length; i++){
-    // if (vowels.includes(arr[i])) { //first way
-    //   count++
-    // }
-    if (vowels.indexOf(arr[i]) >= 0) { //second way
-      count++
-    }
-  }
+  arrayFromStr.forEach(el => vowels.indexOf(el) >= 0 ? count++ : null)
   return count;
 }
 
-console.log(findVowels2('this is a striing'));
-
-// first way
-// const findVowels = str => {
-//   const vowels = /[aeiou]/; //regular expression
-//   return str.split(vowels).length - 1;
-// }
-
-// console.log(findVowels('this is a string'));
-//
-// // second way
-// const findVowels1 = str => {
-//   const vowels = ['a','e','i','o','u'];
-//   const arrayFromStr = str.split('');
-//   let count = 0;
-//   arrayFromStr.forEach(el => vowels.indexOf(el) >= 0 ? count++ : null)
-//   return count;
-// }
-//
-// console.log(findVowels1('this is a string'));
+console.log(findVowels1('this is a string'));
 
 // No Duplicates!
 //   A set is a collection of unique items. A set can be formed from an array by removing all duplicate items.
@@ -64,35 +30,20 @@ console.log(findVowels2('this is a striing'));
 //   [2, 2, 2, 2, 2, 2] ➞ [2]
 //   [5, 10, 15, 20, 25] ➞ [5, 10, 15, 20, 25]
 
+//first way
 const unique = arr => {
-  const uniqueArr = [];
-  for (let i = 0; i<arr.length; i++) {
-    if (!uniqueArr.includes(arr[i])) {
-      uniqueArr.push(arr[i]);
-    }
-  }
-  return uniqueArr;
+  return [...new Set(arr)];
 }
 
 console.log(unique([1, 4, 4, 7, 7, 7]));
 
+//second way
+const unique2 = arr => {
+  return arr.reduce((uniq, item) =>
+    uniq.includes(item) ? uniq : [...uniq, item], []);
+}
 
-
-//
-// //first way
-// const unique = arr => {
-//   return [...new Set(arr)];
-// }
-//
-// console.log(unique([1, 4, 4, 7, 7, 7]));
-//
-// //second way
-// const unique2 = arr => {
-//   return arr.reduce((uniq, item) =>
-//     uniq.includes(item) ? uniq : [...uniq, item], []);
-// }
-//
-// console.log(unique2([1, 4, 4, 7, 7, 7]));
+console.log(unique2([1, 4, 4, 7, 7, 7]));
 
 //
 // Dictionary. Create a function that takes an initial string and an array of words, and returns a
@@ -105,26 +56,12 @@ console.log(unique([1, 4, 4, 7, 7, 7]));
 // dictionary(“tri”, [“triplet”, “tries”, “trip”, “piano”, “tree”]) ➞ [“triplet”, “tries”, trip”]
 // dictionary(“beau”, [“pastry”, “delicious”, “name”, “boring”]) ➞ []
 //
-// const dictionary = (str, arr) => {
-//   let matchArr = [];
-//   arr.forEach(el => el.includes(str) ? matchArr.push(el) : null);
-//   return matchArr;
-// }
-//
-// console.log(dictionary('bu', ['button', 'breakfast', 'border']));
-// console.log(dictionary('tri', ['triplet', 'tries', 'trip', 'piano', 'tree']));
-// console.log(dictionary('beau', ['pastry', 'delicious', 'name', 'boring']));
+const dictionary = (str, arr) => {
+  let matchArr = [];
+  arr.forEach(el => el.includes(str) ? matchArr.push(el) : null);
+  return matchArr;
+}
 
-// const dictionary = (str, arr) => {
-//
-//   const auxArr = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i].includes(str)) {
-//       auxArr.push(arr[i]);
-//     }
-//   }
-//   return auxArr;
-// }
-
-
-console.log(dictionary('ord', ['button', 'breakfast', 'border']));
+console.log(dictionary('bu', ['button', 'breakfast', 'border']));
+console.log(dictionary('tri', ['triplet', 'tries', 'trip', 'piano', 'tree']));
+console.log(dictionary('beau', ['pastry', 'delicious', 'name', 'boring']));
