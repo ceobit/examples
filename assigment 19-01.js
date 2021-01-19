b208672e0a7a17e58457f1e9a35b086073ea0556
Roman Lebedenko <ceobit90@gmail.com>
2021-01-19 13:38:09 +0100

    assignment 19-01

--- assigment 19-01.js
+++ assigment 19-01.js
@@ -3,9 +3,20 @@
 //   of vowels (i.e. in English, “a, e, i, o, u”) in the string.
 //   i.e. findVowels(“this is a string”)
 
+// const findVowels = str => {
+//   const vowels = /[aeiou]/;
+//   return str.split(vowels).length - 1;
+// }
+//
+// console.log(findVowels('this is a string'));
+
+//second way
 const findVowels = str => {
-  const vowels = /[aeiou]/;
-  return str.split(vowels).length - 1;
+  const vowels = ['a','e','i','o','u'];
+  const arrayFromStr = str.split('');
+  let count = 0;
+  arrayFromStr.forEach(el => vowels.indexOf(el) >= 0 ? count++ : null)
+  return count;
 }
 
 console.log(findVowels('this is a string'));
