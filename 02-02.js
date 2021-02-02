b20f200628a352e2cb20d104e3abc1d591767cfb
Roman Lebedenko <ceobit90@gmail.com>
2021-02-02 11:14:19 +0100

    objects

--- 02-02.js
+++ 02-02.js
@@ -6,8 +6,8 @@
 const person2 = {
   name: 'Sofia',
   age: 3,
-  city: 'Hamburg'
-}
+  city: 'Hamburg',
+};
 
 for (const [key, value] of Object.entries(person2)) {
   console.log(`${key}: ${value}`);
@@ -16,7 +16,7 @@ for (const [key, value] of Object.entries(person2)) {
 // Create a function that returns an array of the properties of a javascript object.
 const objProp = person2 => {
   return [key, value] = Object.entries(person2);
-}
+};
 
 console.log(objProp(person2));
 
@@ -29,12 +29,59 @@ console.log(objProp(person2));
 //   "Zain Oil a student in class 48"
 
 const student = {
-  firstName: "Zain",
-  lastName: "Oil",
+  firstName: 'Zain',
+  lastName: 'Oil',
   class: 48,
   print: function() {
-    console.log(`${this.firstName} ${this.lastName} a student in class ${this.class}`);
-  }
+    console.log(
+      `${this.firstName} ${this.lastName} a student in class ${this.class}`);
+  },
 };
 
 student.print();
+
+// Get Values. Create a function that returns an array of all values of an object’s properties.
+//   Examples:
+// getObjectValues({
+//   choice1: "tea",
+//   choice2: "coffee",
+//   choice3: "milk"
+// })
+// Expected output:
+//   ["tea", "coffee", "milk"]
+
+const objChoice = {
+  choice1: 'tea',
+  choice2: 'coffee',
+  choice3: 'milk',
+};
+
+const getObjectValues = obj => {
+  return Object.values(obj);
+};
+
+console.log(getObjectValues(objChoice));
+
+// List Properties. Create a function that returns an array of properties of a javascript object.
+//   Example
+// let student = {
+//   name: "Mike",
+//   class: "4A"
+//   course: "English"
+// }
+// Expected output:
+//   ["name", "class", "course"]
+
+const studentNew = {
+  name: 'Mike',
+  class: '4A',
+  course: 'English'
+}
+
+const getObjectKeys= obj => {
+  return Object.keys(obj);
+};
+
+console.log(getObjectKeys(studentNew));
+
+
