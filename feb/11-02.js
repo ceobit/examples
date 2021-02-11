6c1a809a44680a550613e51537e78f053030e846
Roman Lebedenko <ceobit90@gmail.com>
2021-02-11 13:52:34 +0100

    getter/setter

--- feb/11-02.js
+++ feb/11-02.js
@@ -18,11 +18,51 @@ const car = {
   get data() {
     console.log(`${this.modelName} has number: ${this.modelNumber} and engine capacity =  ${this.engineCapacity}`);
   },
-  set data(props) {
-    [this.modelName, this.modelNumber, this.engineCapacity] = [...props.split(',')];
+  set data({...props}) {
+    Object.assign(this, props)
   }
 }
 
 car.data;
-car.data = 'Ford,12123243545,140';
+car.data = {modelName: 'Ford', modelNumber: '12123243545', engineCapacity: 140};
+
 car.data;
+
+
+// classes
+class Animals{
+  #kind;
+  constructor(kind, hometown){
+    this.#kind = kind;
+    this.hometown = hometown;
+  }
+
+  display() {
+    console.log(`${this.#kind} ${this.hometown}`);
+  }
+}
+
+const cat = new Animals('cat', 'home');
+cat.display();
+
+class Tv {
+  constructor(_brand, _channel = 1, _volume = 10) {
+    this.brand = _brand;
+    this.channel = _channel;
+    this.volume = _volume;
+  }
+  incVolume(volume) {
+    this.volume += volume;
+  }
+  decVolume(volume) {
+    volume > this.volume ? console.log(`volume should be less than ${this.volume}`) : this.volume -= volume;
+  }
+  rest() {
+    this.volume = 10;
+  }
+}
+
+const lg = new Tv('LG', 400, 50);
+lg.incVolume(230);
+lg.decVolume(500);
+lg.rest();
