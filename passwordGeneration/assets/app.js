a86c729f9992097389168c9b6fa1da13f12b028d
Roman Lebedenko <ceobit90@gmail.com>
2021-04-28 13:19:03 +0200

    desktop is done

--- passwordGeneration/assets/app.js
+++ passwordGeneration/assets/app.js
@@ -5,12 +5,22 @@ const range = document.querySelector('.form-range');
 const rangeValue = document.querySelector('.range-value');
 const btn = document.querySelector('.btn');
 const password = document.querySelector('.password');
-
+const complexity = document.querySelector('.form-check-input');
+const note = document.querySelector('.note');
 
 const passGenerator = (e, length) => {
   let pass = '';
-  const charSet =
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,@#!?$';
+  const def = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,@#!?$';
+  let charSet = def;
+
+
+  if (complexity.checked) {
+    charSet += 'АБВГДЕЖЗИКЛМНОПРСТУЫЙЪЬФЭЮЯ';
+    note.textContent = 'Yes now you have Russian letters here!!!';
+  } else {
+    charSet = def;
+    note.textContent = '';
+  }
 
   for (let i = 0; i < length; i++) {
     pass += charSet.charAt(Math.floor(Math.random() * charSet.length));
