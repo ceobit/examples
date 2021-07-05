a2ae7dafb2b776f6540884ca9087b4aa1bec4691
Roman Lebedenko <ceobit90@gmail.com>
2021-07-05 15:58:56 +0700

    complete

---
+++ react-practice/class-components/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
---
+++ xo/2343546.js
@@ -0,0 +1,8 @@
+'use strick mode'
+
+arr = [];
+
+arr = 0;
+
+static
+private
